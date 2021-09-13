828c9923397bae88282948d8fd1a286c11ea54e3
Danielle <danielleajala@gmail.com>
2021-09-13 18:54:24 -0500

    navbar scratch

---
+++ src/main/frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function navbar() {
+
+    return (
+        <div className="nav-bar">
+            <div>New appt</div>
+            <div>New client</div>
+            <div>Patient info</div>
+            <div>Provider schedule</div>
+        </div>
+    )
+}
