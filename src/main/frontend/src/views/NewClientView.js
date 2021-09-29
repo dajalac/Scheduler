c5520cbc75611512c2fbc5b3ed53b5e58336b7af
Danielle <danielleajala@gmail.com>
2021-09-29 12:44:19 -0500

    new client done

--- src/main/frontend/src/views/NewClientView.js
+++ src/main/frontend/src/views/NewClientView.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NewClientForm from '../components/client/NewClientForm';
+import Button from '@mui/material/Button';
 
 export default function NewClient() {
     return (
         <div>
+        <div className="App-center-div">
             <NewClientForm/>
+        </div>
+        <Button variant="contained" color="success" size="large"> Save </Button>
         </div>
     )
 }
