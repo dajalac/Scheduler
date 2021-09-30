58a07a1abfc30e0c132777054016afe56aacc924
Danielle <danielleajala@gmail.com>
2021-09-30 15:51:26 -0500

    clicable nav tabs

--- src/main/frontend/src/components/navbar/Sidebar.js
+++ src/main/frontend/src/components/navbar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
 import InfoIcon from '@mui/icons-material/Info';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
@@ -10,33 +10,38 @@ import './Sidebar.css'
 
 export default function sidebar() {
 
+
     return (
-        <div className= 'side-bar'>
-        <nav>
-            <ul className = "side-bar-items">
-            <li className="side-bar-links">
-                    <HomeIcon className="side-bar-menu-icons"/>
-                    <Link to ='/'>Home</Link>
-                </li>
-                <li className="side-bar-links">
-                    <FolderIcon  className="side-bar-menu-icons"/>
-                   <Link to= '/newAppt'> Manage Appoitments </Link>
-                </li>
-                <li className="side-bar-links">
-                    <PersonAddIcon className="side-bar-menu-icons" />
-                    <Link to ='/newClient'> New client </Link>
-                </li>
-                {/*
-                <li className="side-bar-links">
-                    <InfoIcon className="side-bar-menu-icons"/>
-                    <Link to ='/clientInfo'> Client info</Link>
-                </li>*/ }
-                <li className="side-bar-links">
-                    <DateRangeIcon className="side-bar-menu-icons"/>
-                     <Link to ='/providerSchedule'>Provider schedule</Link>
-                </li>
-            </ul>
-        </nav>
+        <div className='side-bar'>
+            <nav>
+                <ul className="side-bar-items">
+                    <NavLink exact to='/' style={{ textDecoration: 'none' }} activeClassName="side-bar-items-active-tab">
+                        <li className="side-bar-links">
+                            <HomeIcon className="side-bar-menu-icons" />
+                            <div>Home </div>
+                        </li>
+                    </NavLink>
+
+                    <NavLink  exact to='/newAppt' style={{ textDecoration: 'none' }} activeClassName="side-bar-items-active-tab">
+                        <li className="side-bar-links">
+                            <FolderIcon className="side-bar-menu-icons" />
+                            <div>Manage Appoitments</div>
+                        </li>
+                    </NavLink>
+                    <NavLink exact to='/newClient' style={{ textDecoration: 'none' }} activeClassName="side-bar-items-active-tab">
+                        <li className="side-bar-links">
+                            <PersonAddIcon className="side-bar-menu-icons" />
+                            <div> New client </div>
+                        </li>
+                    </NavLink>
+                    <NavLink exact to='/providerSchedule' style={{ textDecoration: 'none' }} activeClassName="side-bar-items-active-tab">
+                        <li className="side-bar-links">
+                            <DateRangeIcon className="side-bar-menu-icons" />
+                            <div>Provider schedule</div>
+                        </li>
+                    </NavLink>
+                </ul>
+            </nav>
         </div>
 
     )
--- src/main/frontend/src/views/AppointmentsHisotoryView.js
+++ src/main/frontend/src/views/AppointmentsHisotoryView.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import DisplayClients from '../components/client/DisplayClients';
 import ApptHistory from '../components/miscComponents/ApptHistory';
+import PrintIcon from '@mui/icons-material/Print';
 
 import './AppointmentsHisotoryView.css'
 
+
 export default function AppointmentsHisotoryView() {
+
     return (
-        <div className="AppointmentsHistoryView">
+        <div className="AppointmentsHistoryView" id="printableArea">
              <DisplayClients />
+             <div className="AppointmentsHistoryView-print" onClick={() => window.print()}>
+             <PrintIcon />   
+             print</div> 
              <ApptHistory/>
         </div>
     )
